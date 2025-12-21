da544df992c1db33ae906d3d5ca05de12e4613e8
marcgrue <marcgrue@gmail.com>
2025-12-21 21:01:48 +0100

    Molecule v0.29.0 Flyway migration

--- molecule-basic/project/plugins.sbt
+++ molecule-basic/project/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("org.scalamolecule" % "sbt-molecule" % "1.23.0")
+addSbtPlugin("org.scalamolecule" % "sbt-molecule" % "1.24.0")
--- molecule-basic/src/main/scala/app/domain/Person.scala
+++ molecule-basic/src/main/scala/app/domain/Person.scala
@@ -2,7 +2,7 @@ package app.domain
 
 import molecule.DomainStructure
 
-object Person extends DomainStructure {
+trait Person extends DomainStructure {
   trait Person:
     val name = oneString
     val age  = oneInt
